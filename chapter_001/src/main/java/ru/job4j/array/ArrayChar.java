0701807ece3934c392c42d83ae13a9dfb44c2f8b
AleksandrTyotkin <Tyotkin@list.ru>
2019-10-15 18:39:50 +0500

    6.4. Слово начинается с ...[#162674] исправления

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,13 +3,13 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
+        boolean result = true;
         int index = pref.length;
         int i = 0;
         for (; i < index; i++) {
-            if (word[i] == pref[i])
-                result = true;
-            else return result = false;
+            if (word[i] != pref[i])
+                result = false;
+
 
         }
         return result;
