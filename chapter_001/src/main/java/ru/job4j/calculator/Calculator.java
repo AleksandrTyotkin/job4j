6b05a5f18235e6eb0489338073639788e2f6c812
AleksandrTyotkin <Tyotkin@list.ru>
2019-10-23 20:57:28 +0500

    5.7. JavaDoc. Code convention[#162665] изм 231019

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -7,22 +7,45 @@ package ru.job4j.calculator;
  */
 
 public class Calculator {
-
+    /**
+     * Method add.
+     *
+     * @param name first, second.
+     * @return add first + double.
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
 
+    /**
+     * Method div.
+     *
+     * @param name first, second.
+     * @return div first / double.
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
 
+    /**
+     * Method multyply.
+     *
+     * @param name first, second.
+     * @return multyply first * double.
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
 
+    /**
+     * Method subtrack.
+     *
+     * @param name first, second.
+     * @return subtrack first - double.
+     */
     public static void subtrack(double first, double second) {
         double result = first - second;
         System.out.println(first + "-" + second + " = " + result);
@@ -34,29 +57,13 @@ public class Calculator {
      * @param args - args.
      */
     public static void main(String[] args) {
-        /**
-         * Method add.
-         * @param name first, second.
-         * @return add first + double.
-         */
+
         add(1, 1);
-        /**
-         * Method div.
-         * @param name first, second.
-         * @return div first / double.
-         */
+
         div(4, 2);
-        /**
-         * Method multyply.
-         * @param name first, second.
-         * @return multyply first * double.
-         */
+
         multiply(2, 1);
-        /**
-         * Method subtrack.
-         * @param name first, second.
-         * @return subtrack first - double.
-         */
+
         subtrack(10, 5);
 
     }
